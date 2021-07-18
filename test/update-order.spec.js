4246c984f2bece30caa903b812b42f73d0db044a
Igor Kulishov <igorkulishov@gmail.com>
2021-07-18 17:46:11 -0400

    EC-67-UTs: CICD workflows

--- test/update-order.spec.js
+++ test/update-order.spec.js
@@ -11,28 +11,25 @@ AWSMock.setSDKInstance(AWS);
 AWSMock.mock('DynamoDB.DocumentClient', 'update', updateDBFunc);
 AWS.config.update({ region: "us-east-1" });
 const updateOrder = require('../orders/update-order');
-
 describe('update order', () => {
-    const eventMock = { body: JSON.stringify({
-            orderDetails: {
-                totalAmount: 78.98,
-                orderNumber: "689a43817e711f530598bef44f078700",
-                orderToken: null,
-                totalQuantity: 3,
-                paymentPlaced: null,
-                paymentId: null,
-                itemList: []
-            },
-            orderStatus: 'not_checked'
-        }), pathParameters: { id: '1234567' }};
-
-    beforeEach(function() {});
 
     afterEach(function() {
         AWSMock.restore('DynamoDB.DocumentClient');
     });
 
     it('if dynamoDB update was called', () => {
+        const eventMock = { body: JSON.stringify({
+                orderDetails: {
+                    totalAmount: 78.98,
+                    orderNumber: "689a43817e711f530598bef44f078700",
+                    orderToken: null,
+                    totalQuantity: 3,
+                    paymentPlaced: null,
+                    paymentId: null,
+                    itemList: []
+                },
+                orderStatus: 'not_checked'
+            }), pathParameters: { id: '1234567' }};
         updateOrder.update(eventMock, {}, mockLambdaCallback);
         expect(mockLambdaCallback.calledOnce).to.be.true;
     });
