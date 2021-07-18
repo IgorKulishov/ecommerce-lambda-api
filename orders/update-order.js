ea37d9e003140f9f0ed0926e43a7b9993f27a823
Igor Kulishov <igorkulishov@gmail.com>
2021-07-18 19:50:57 -0400

    EC-67-UTs: CICD workflows

--- orders/update-order.js
+++ orders/update-order.js
@@ -41,6 +41,7 @@ module.exports.update = (event, context, callback) => {
     // handle potential errors
     console.log('DB update');
     if (error) {
+      console.log('DB update error: ');
       console.error(error);
       callback({
         statusCode: error.statusCode || 501,
