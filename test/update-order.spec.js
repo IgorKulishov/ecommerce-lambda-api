c65a8c2e6c1bb9ced0b45c6a68d6f3afd1ab84d7
Igor Kulishov <igorkulishov@gmail.com>
2021-07-18 18:41:06 -0400

    EC-67-UTs: CICD workflows

--- test/update-order.spec.js
+++ test/update-order.spec.js
@@ -1,37 +1,37 @@
-// 'use strict';
-// const expect = require('chai').expect;
-// const sinon = require('sinon');
-// const AWS = require('aws-sdk');
-// const AWSMock = require('aws-sdk-mock');
-// const updateDBFunc= (params, queryCallback) => {
-//     queryCallback(null, { Items: 'successfully update item in database' });
-// };
-// const mockLambdaCallback = sinon.spy();
-// AWSMock.setSDKInstance(AWS);
-// AWSMock.mock('DynamoDB.DocumentClient', 'update', updateDBFunc);
-// AWS.config.update({ region: "us-east-1" });
-// const updateOrder = require('../orders/update-order');
-// describe('update order', () => {
-//     const eventMock = {
-//         body: JSON.stringify({
-//             orderDetails: {
-//                 totalAmount: 78.98,
-//                 orderNumber: "689a43817e711f530598bef44f078700",
-//                 orderToken: null,
-//                 totalQuantity: 3,
-//                 paymentPlaced: null,
-//                 paymentId: null,
-//                 itemList: []
-//             },
-//             orderStatus: 'not_checked'
-//         }), pathParameters: { id: '1234567' }};
-//
-//     afterEach(function() {
-//         AWSMock.restore('DynamoDB.DocumentClient');
-//     });
-//
-//     it('if dynamoDB update was called', () => {
-//         updateOrder.update(eventMock, {}, mockLambdaCallback);
-//         expect(mockLambdaCallback.calledOnce).to.be.true;
-//     });
-// });
+'use strict';
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const AWS = require('aws-sdk');
+const AWSMock = require('aws-sdk-mock');
+const updateDBFunc= (params, queryCallback) => {
+    queryCallback(null, { Items: 'successfully update item in database' });
+};
+const mockLambdaCallback = sinon.spy();
+AWSMock.setSDKInstance(AWS);
+AWSMock.mock('DynamoDB.DocumentClient', 'update', updateDBFunc);
+AWS.config.update({ region: "us-east-1" });
+const updateOrder = require('../orders/update-order');
+describe('update order', () => {
+    const eventMock = {
+        body: JSON.stringify({
+            orderDetails: {
+                totalAmount: 78.98,
+                orderNumber: "689a43817e711f530598bef44f078700",
+                orderToken: null,
+                totalQuantity: 3,
+                paymentPlaced: null,
+                paymentId: null,
+                itemList: []
+            },
+            orderStatus: 'not_checked'
+        }), pathParameters: { id: '1234567' }};
+
+    afterEach(function() {
+        AWSMock.restore('DynamoDB.DocumentClient');
+    });
+
+    it('if dynamoDB update was called', () => {
+        updateOrder.update(eventMock, {}, mockLambdaCallback);
+        expect(mockLambdaCallback.calledOnce).to.be.true;
+    });
+});
